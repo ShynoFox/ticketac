087e78158e4182eef2d13143b9e0c01535f0b7ee
Mélanie Gouez <m.gouez@outlook.com>
2022-04-14 17:53:52 +0200

    navig2

--- models/users.js
+++ models/users.js
@@ -1,5 +1,12 @@
 var mongoose = require('mongoose')
 
+var journeySchema = mongoose.Schema({
+    departure: String,
+    arrival: String,
+    date: Date,
+    departureTime: String,
+    price: Number,
+  });
 var userSchema = mongoose.Schema({
     name: String,
     firstname:String,
